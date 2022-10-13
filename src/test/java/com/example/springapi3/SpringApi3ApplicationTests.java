51ac5f49f008ec20034ffba777649fc2e8d576d9
Quan Tuan <dactuanbka@gmail.com>
2022-10-13 11:08:22 +0700

    init first authentication web

---
+++ src/test/java/com/example/springapi3/SpringApi3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springapi3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApi3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
