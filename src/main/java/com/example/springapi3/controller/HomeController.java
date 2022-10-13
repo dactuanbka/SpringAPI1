322b6c95b6059c4ca9d45c3513fde77db402cb61
Quan Tuan <dactuanbka@gmail.com>
2022-10-13 11:28:22 +0700

    change to index.html

--- src/main/java/com/example/springapi3/controller/HomeController.java
+++ src/main/java/com/example/springapi3/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String homePage() {
-        return "MyImage";
+        return "index";
     }
 
 }
